c3c05c013b42672ebb291e10ab8fbcbfccb14cb0
Léon Brocard <acme@astray.com>
2000-08-18 13:02:20 +0000

    Import of LBROCARD/GraphViz-0.05 from CPAN.
    gitpan-cpan-distribution: GraphViz
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         LBROCARD/GraphViz-0.05.tar.gz
    gitpan-cpan-author:       LBROCARD
    gitpan-cpan-maturity:     released

--- GraphViz.pm
+++ GraphViz.pm
@@ -3,9 +3,11 @@ package GraphViz;
 use strict;
 use vars qw($VERSION $name_counter);
 use Carp;
+use IPC::Run qw(run);
+use vars qw($AUTOLOAD);
 
 # This is incremented every time there is a change to the API
-$VERSION = '0.04';
+$VERSION = '0.05';
 
 
 =head1 NAME
@@ -49,21 +51,19 @@ Prints the following:
 
 =head1 DESCRIPTION
 
-This provides an object-orientated interface to generating the ".dot"
-files needed by the "dotneato" program in the GraphViz project
-(http://www.graphviz.org/). It constructs a file which can then be
-used to draw directed graphs in a variety of formats (Postscript, GIF,
-etc.)
+This modules provides an interface to layout and generate images of
+directed graphs in a variety of formats (PostScript, PNG, etc.) using
+the "dotneato" program from the GraphViz project
+(http://www.graphviz.org/).
 
-At the moment this is a very simple library (it is a very simple
-format). Some features of the format are not currently implemented,
-such as graph attributes. Feature requests are welcome!
-
-I am not going to document the ".dot" file format: the node and edge
-attributes are available in the "dot" manpage, so you should check
-that out if you want to use non-default attributes.
+At the moment this is a fairly simple library. Some features of
+dotneato are not currently implemented, such as graph
+attributes. Feature requests are welcome!
 
 =head1 METHODS
+
+=cut
+
 
 =head2 new
 
@@ -153,7 +153,197 @@ sub add_edge {
 }
   
 
-sub as_text {
+=head2 as_canon, as_text, as_gif etc. methods
+
+There are a number of methods which generate input for dotneato or
+output the graph in a variety of formats.
+
+=over 4
+
+=item as_canon
+
+The as_canon method returns the canonical dotneato file which
+corresponds to the graph. It does not layout the graph - every other
+as_* method does.
+
+  print $g->as_canon;
+
+
+  # prints out something like:
+  digraph test {
+      node [	label = "\N" ];
+      London [label=London];
+      Paris [label="City of\nlurve"];
+      New_York [label="New York"];
+      London -> Paris;
+      London -> New_York [label=Far];
+      Paris -> London;
+  }
+
+=item as_text
+
+The as_text method returns text which is a layed-out dotneato-format file.
+
+  print $g->as_text;
+
+  # prints out something like:
+  digraph test {
+      node [	label = "\N" ];
+      graph [bb= "0,0,162,134"];
+      London [label=London, pos="33,116", width="0.89", height="0.50"];
+      Paris [label="City of\nlurve", pos="33,23", width="0.92", height="0.62"];
+      New_York [label="New York", pos="123,23", width="1.08", height="0.50"];
+      London -> Paris [pos="e,27,45 28,98 26,86 26,70 27,55"];
+      London -> New_York [label=Far, pos="e,107,40 49,100 63,85 84,63 101,46", lp="99,72"];
+      Paris -> London [pos="s,38,98 39,92 40,78 40,60 39,45"];
+  }
+
+=item as_ps
+
+Returns a string which contains a layed-out PostScript-format file.
+
+  print $g->as_ps;
+
+=item as_hpgl
+
+Returns a string which contains a layed-out HP pen plotter-format file.
+
+  print $g->as_hpgl;
+
+=item as_pcl
+
+Returns a string which contains a layed-out Laserjet printer-format file.
+
+  print $g->as_pcl;
+
+=item as_mif
+
+Returns a string which contains a layed-out FrameMaker graphics-format file.
+
+  print $g->as_mif;
+
+=item as_pic
+
+Returns a string which contains a layed-out PIC-format file.
+
+  print $g->as_pic;
+
+=item as_gd
+
+Returns a string which contains a layed-out GD-format file.
+
+  print $g->as_gd;
+
+=item as_gd2
+
+Returns a string which contains a layed-out GD2-format file.
+
+  print $g->as_gd2;
+
+=item as_gif
+
+Returns a string which contains a layed-out GIF-format file.
+
+  print $g->as_gif;
+
+=item as_jpeg
+
+Returns a string which contains a layed-out JPEG-format file.
+
+  print $g->as_jpeg;
+
+=item as_png
+
+Returns a string which contains a layed-out PNG-format file.
+
+  print $g->as_png;
+
+=item as_wbmp
+
+Returns a string which contains a layed-out Windows BMP-format file.
+
+  print $g->as_wbmp;
+
+=item as_ismap
+
+Returns a string which contains a layed-out HTML client-side image map
+format file.
+
+  print $g->as_ismap;
+
+=item as_imap
+
+Returns a string which contains a layed-out HTML server-side image map
+format file.
+
+  print $g->as_imap;
+
+=item as_vrml
+
+Returns a string which contains a layed-out VRML-format file.
+
+  print $g->as_vrml;
+
+=item as_vtx
+
+Returns a string which contains a layed-out VTX (Visual Thought)
+format file.
+
+  print $g->as_vtx;
+
+=item as_mp
+
+Returns a string which contains a layed-out MetaPost-format file.
+
+  print $g->as_mp;
+
+=item as_fig
+
+Returns a string which contains a layed-out FIG-format file.
+
+  print $g->as_fig;
+
+=item as_svg
+
+Returns a string which contains a layed-out SVG-format file.
+
+  print $g->as_svg;
+
+=item as_plain
+
+Returns a string which contains a layed-out simple-format file.
+
+  print $g->as_plain;
+
+=back
+
+=cut
+
+# Generate magic methods to save typing
+
+sub AUTOLOAD {
+  my $self = shift;
+  my $type = ref($self)
+    or croak "$self is not an object";
+  
+  my $name = $AUTOLOAD;
+  $name =~ s/.*://;   # strip fully-qualified portion
+
+  if ($name eq 'as_text') {
+    $name = "as_dot";
+  }
+
+  if ($name =~ /^as_(ps|hpgl|pcl|mif|pic|gd|gd2|gif|jpeg|png|wbmp|ismap|imap|vrml|vtx|mp|fig|svg|dot|canon|plain)$/) {
+    return $self->_as_generic('-T' . $1);
+  }
+  
+  croak "Method $name not defined!";
+}
+
+
+# Generate the actual dot text
+
+sub _as_debug {
   my $self = shift;
 
   my $dot;
@@ -175,11 +365,32 @@ sub as_text {
   return $dot;
 }
 
+
+# Call neato with the dotneato input text and any parameters
+
+sub _as_generic {
+  my $self = shift;
+
+  my $dot = $self->_as_debug;
+
+  my $out;
+  run ['dotneato', @_], \$dot, \$out;
+
+  return $out;
+}
+
+
+# Quote a node/edge name using dotneato's quoting rules
+
 sub _quote_name {
   my $name = shift;
   $name =~ s|[ :]|_|g;
   return $name;
 }
+
+
+# Return the attributes of a node or edge as a dotneato attribute
+# string
 
 sub _attributes {
   my $thing = shift;
@@ -196,7 +407,7 @@ sub _attributes {
   }
 
   if (@attributes) {
-    return ' [' . (join ', ', @attributes) . "]";
+    return ' [' . (join ', ', sort @attributes) . "]";
   } else {
     return "";
   }
--- test.pl
+++ test.pl
@@ -3,16 +3,19 @@
 use strict;
 use Test;
 
-BEGIN { plan tests => 3 }
+BEGIN { plan tests => 5 }
 
 use GraphViz;
 
+# Does it load?
 ok(1); # 1
 
 my $g = GraphViz->new();
 
+# Does the constructor work?
 ok(defined($g), 1); #2
 
+# Let's build a simple graph
 $g->add_node({ name => 'London'});
 $g->add_node({ name => 'Paris', label => 'City of\nlurve'});
 $g->add_node({ name => 'New York'});
@@ -37,7 +40,35 @@ my $expect_text = qq|digraph test {
 }
 |;
 
-ok($g->as_text, $expect_text); #3
+# Does is generate the right .dot text?
+ok($g->_as_debug, $expect_text); #3
 
+$expect_text = q|digraph test {
+	node [	label = "\N" ];
+	London [label=London];
+	Paris [label="City of\nlurve"];
+	New_York [label="New York"];
+	London -> Paris;
+	London -> New_York [label=Far];
+	Paris -> London;
+}
+|;
 
+# Does it run dotneato and return the canonical representation
+ok($g->as_canon, $expect_text); #4
+
+$expect_text = q|digraph test {
+	node [	label = "\N" ];
+	graph [bb= "0,0,162,134"];
+	London [label=London, pos="33,116", width="0.89", height="0.50"];
+	Paris [label="City of\nlurve", pos="33,23", width="0.92", height="0.62"];
+	New_York [label="New York", pos="123,23", width="1.08", height="0.50"];
+	London -> Paris [pos="e,27,45 28,98 26,86 26,70 27,55"];
+	London -> New_York [label=Far, pos="e,107,40 49,100 63,85 84,63 101,46", lp="99,72"];
+	Paris -> London [pos="s,38,98 39,92 40,78 40,60 39,45"];
+}
+|;
+
+# Does it run dotneato and lay things out properly?
+ok($g->as_text, $expect_text); #5
 
